d5445247ec78f841c511eeed41da0bd892d2f05d
AADITYA PRASAD <122255434+onenonlybotz@users.noreply.github.com>
2023-01-15 19:54:14 +0530

    Update luna.py

--- luna.py
+++ luna.py
@@ -53,7 +53,7 @@ async def type_and_send(message):
 @luna.on_message(filters.command("repo") & ~filters.edited)
 async def repo(_, message):
     await message.reply_text(
-        "[GitHub](https://github.com/thehamkercat/LunaChatBot)"
+        "[GitHub](https://t.me/saikostar_xd)"
         + " | [Group](t.me/PatheticProgrammers)",
         disable_web_page_preview=True,
     )
